a43d1a8a18018fed1cc181d9359c6fa640d801e5
HamaHs <lusuy1995@gmail.com>
2022-08-05 16:33:56 +0300

    added scripts for slides in the Feedback section

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+
+const checkClass = document.querySelector('.swiper');
+
+if (checkClass) {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoHeight: true,
+    direction: 'horizontal',
+    loop: true,
+    speed: 700,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.customers__paginations',
+      bulletClass: 'customers__pagination-bullet',
+      bulletActiveClass: 'customers__controls-item_active',
+      clickable: true,
+      type: 'custom',
+    },
+
+    // If we need autoplay
+    autoplay: {
+      delay: 5000,
+    },
+    
+    // Responsive brackpoints
+    breakpoints: {
+      // when window width is >= 1024px
+      1024: {
+        allowTouchMove: false,
+      },
+    },
+  });
+}
